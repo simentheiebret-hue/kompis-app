ac47fde5beddad1b062e5a69d7975ca748aa0724
Simen <simentheiebret@gmail.com>
2026-02-05 19:12:49 +0100

    Initial Commit

---
+++ Kompis-App/Kompis_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kompis_AppApp.swift
+//  Kompis-App
+//
+//  Created by Simen Theie Bretvik on 05/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Kompis_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
